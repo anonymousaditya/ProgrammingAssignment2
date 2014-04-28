79f8b29035018dedcffe29c26443a2da2054769c
anonymousaditya <anonymousaditya@users.noreply.github.com>
2014-04-28 07:58:59 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,19 +7,29 @@ makeCacheMatrix <- function(x = matrix()) {
         set <- function(y) {
                 x <<- y
                 m <<- NULL
-        }
-        get <- function() x
-        setinverse <- function(inverse) m <<- mean
-        getmean <- function() m
+        } ##Setting the value of the matrix
+        get <- function() x ##Getting the value of the Matrix
+        setinverse <- function(inverse) m <<- inverse ##Set the value of inverse matrix
+        getinverse <- function() m ##Getting the inverse value of matrix
         list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
+install.packages(MASS)## Installing the package of Mass which has the function for getting the inverse of a matrix
+## The following function solves and returns the inverse of the special matrix "makeCacheMatrix" 
+## created in the previous function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()## Gets the inverse of data from cache of previous function
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        } ##Checks if the inverse has already been calculated and in case it has been calculated, returns it
+        data <- x$get() ## If the inverse is not calcultaed it gets the data from the previous function
+        m <- ginv(data, ...)## Calculates the Inverse using ginv function from the MASS package
+        x$setinverse(m)## Assisgns the inverse value to the set inverse function of the previous function thus 
+        ## recursively calling it back in the previous If loop to be printed 
+        m ##simply prints out inverse
 }
