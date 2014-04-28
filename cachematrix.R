13d9b972d56049f3f5d1620c806a79cb140ad606
anonymousaditya <anonymousaditya@users.noreply.github.com>
2014-04-28 07:41:15 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This Function takes in the value of an invertible Matrix, stores it and then returns the inverse of the matrix
 
-## Write a short comment describing this function
+## takes in the value of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 
 }
 
